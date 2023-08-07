e5c05b9967edf3886141b940c750cff0ba77b603
Crowdbotics <engineering@crowdbotics.com>
2023-08-07 23:24:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shrill-smoke-dev-107850",
+  url: "https://shrill-smoke-dev-107850.botics.co",
+  api: "https://shrill-smoke-dev-107850.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
